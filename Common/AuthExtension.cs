1a178a8938c00e7e1bbfb62132fc436fef4976b0
YZ0519 <yikzhang0519@gmail.com>
2026-05-25 14:22:39 +0800

    Local deployment complete

--- Common/AuthExtension.cs
+++ Common/AuthExtension.cs
@@ -17,6 +17,7 @@ namespace Common
                     ValidIssuers = [
                         "http://localhost:6001/realms/overflow",
                         "http://keycloak/realms/overflow",
+                        "http://keycloak:8080/realms/overflow",
                         "http://id.overflow.local/realms/overflow",
                         "https://id.overflow.local/realms/overflow"
                     ],
